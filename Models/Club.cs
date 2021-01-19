67c6917f1e0d20e8ecd655d37d3d919de4004751
tinasche <tinashec93@live.com>
2021-01-19 20:22:30 +0200

    Feat(Models): Added a Club Model.

---
+++ Models/Club.cs
@@ -0,0 +1,9 @@
+namespace BookDepo.Models
+{
+    public class Club
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Affiliation { get; set; }
+    }
+}
--- Models/Patron.cs
+++ Models/Patron.cs
@@ -1,13 +1,25 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookDepo.Models
 {
     public class Patron
     {
+        [Key]
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(20)]
         public string FirstName { get; set; }
+        
+        [Required]
+        [MaxLength(20)]
         public string LastName { get; set; }
+ 
+        [Required]
+        [MaxLength(50)]
         public string Email { get; set; }
+
         public DateTime CreatedOn { get; set; }
     }
 }
